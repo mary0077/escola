bf32a98872acc0308620c9e27c9a1f9421778cda
mary0077 <marianagomes201@hotmail.com>
2024-11-08 12:20:49 -0300

    iniciação da exibição dos alunos cadastrados no front, organização de integrações

--- routes/panel.js
+++ routes/panel.js
@@ -4,25 +4,23 @@ const Aluno = require('../models/aluno');
 const Turma = require('../models/turma');
 const { setRandomFallback } = require('bcryptjs');
 
-router.get('/main', async (req, res)=>{
-    try{
+router.get('/main', async (req, res) => {
+    try {
         const studentData = await Aluno.findAll();
         const classData = await Turma.findAll();
 
         const studentDataArr = studentData.map(studentData => studentData.dataValues);
         const classDataArr = classData.map(classData => classData.dataValues);
 
-        //console.log(studentData);
-
         let data = {
-            students : studentDataArr,
-            class: classDataArr
-        }
+            students: studentDataArr,
+            rooms: classDataArr
+        };
 
-        res.render('main', {data: data});
-    }catch(err){
+        res.render('main', { data: data });
+    } catch (err) {
         console.log(err);
     }
-})
+});
 
 module.exports = router;
